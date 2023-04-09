45686bb8c66f90be6e9e42d30e6835ed88ce6781
Mason Bulling <42184555+mbulling@users.noreply.github.com>
2023-04-09 16:46:42 -0400

    firebase messages

--- lib/message_view.dart
+++ lib/message_view.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'shared_structs.dart';
 
 class MessageView extends StatefulWidget {
-  final List<Message> messages;
+  List<Message> messages;
 
   MessageView({Key? key, required this.messages}) : super(key: key);
 
@@ -11,26 +11,12 @@ class MessageView extends StatefulWidget {
 }
 
 class _MessageViewState extends State<MessageView> {
-  List<Message> messages = [
-    Message(
-        author: "me",
-        time: "now",
-        content: "hello world",
-        zone: "north campus"),
-    Message(
-        author: "me",
-        time: "now",
-        content: "goodbye world",
-        zone: "north campus"),
-    Message(author: "me", time: "now", content: "chirp", zone: "north campus"),
-    Message(author: "mason", time: "now", content: "bark", zone: "north campus")
-  ];
   TextEditingController _textController = TextEditingController();
 
   void _sendMessage() {
     if (_textController.text.isNotEmpty) {
       setState(() {
-        messages.add(Message(
+        widget.messages.add(Message(
             author: "me",
             time: "now",
             content: _textController.text,
@@ -48,18 +34,18 @@ class _MessageViewState extends State<MessageView> {
         children: <Widget>[
           Expanded(
             child: ListView.builder(
-              itemCount: messages.length,
+              itemCount: widget.messages.length,
               itemBuilder: (BuildContext context, int index) {
                 return Padding(
                   padding: EdgeInsets.all(8.0),
                   child: Align(
-                    alignment: messages[index].author == 'me'
+                    alignment: widget.messages[index].author == 'me'
                         ? Alignment.centerRight
                         : Alignment.centerLeft,
                     child: Container(
                       padding: EdgeInsets.all(16.0),
                       decoration: BoxDecoration(
-                        color: messages[index].author == 'me'
+                        color: widget.messages[index].author == 'me'
                             ? Color(
                                 0xFF023258) // Set your sent message background color
                             : Colors
@@ -67,7 +53,7 @@ class _MessageViewState extends State<MessageView> {
                         borderRadius: BorderRadius.circular(16.0),
                       ),
                       child: Text(
-                        messages[index].content,
+                        widget.messages[index].content,
                         style: TextStyle(
                           fontSize: 16.0,
                           color: Colors.white, // Set your message text color
