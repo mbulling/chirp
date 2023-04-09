4ac553abac7e1a47a1b7a81f4b3764cb9bb1792d
andrew <asc269@cornell.edu>
2023-04-09 15:42:36 -0400

    Replace firebase init with initializeFirebase in lib.main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,11 @@ import 'saved_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'common.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
+  await initializeFirebase();
   runApp(MyApp());
 }
 
