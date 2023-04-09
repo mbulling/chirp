15e4e6aa3274e10f03539c5a06da46d0e411f460
andrew <asc269@cornell.edu>
2023-04-09 17:16:04 -0400

    Remove unused imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
 import 'home_page.dart';
 import 'user_profile.dart';
 import 'saved_page.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'common.dart';
 import 'dart:math';
 
--- lib/shared_structs.dart
+++ lib/shared_structs.dart
@@ -1,5 +1,3 @@
-import 'package:meta/meta.dart';
-
 class Message {
   Message(
       {required this.content,
