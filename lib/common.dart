927a9583d919d90c86c386bb3ece254eadd8d841
andrew <asc269@cornell.edu>
2023-04-09 16:01:09 -0400

    Delete commented out message class

--- lib/common.dart
+++ lib/common.dart
@@ -25,28 +25,3 @@ Future<List<Message>> getMessages() async {
   }
   return messages;
 }
-
-
-// class Message {
-//   Message(
-//       {required this.content,
-//       required this.zone,
-//       required this.author,
-//       required this.time});
-
-//   Message.fromJson(Map<String, Object?> json)
-//       : this(
-//             content: json["content"] as String,
-//             zone: json["zone"] as String,
-//             author: json["author"] as String,
-//             time: json["time"] as String);
-
-//   final String content;
-//   final String zone;
-//   final String author;
-//   final String time;
-
-//   Map<String, Object?> toJson() {
-//     return {'message': content, 'zone': zone, 'author': author, 'time': time};
-//   }
-// }
