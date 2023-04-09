789e4647fe56dbd297d897ff807bd150b2a433c3
Kevin Le <kl626@cornell.edu>
2023-04-09 17:13:48 -0400

    Add identifier to messages

--- lib/message_view.dart
+++ lib/message_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'shared_structs.dart';
+import 'user_profile.dart';
+import 'dart:convert';
 
 class MessageView extends StatefulWidget {
   final List<Message> messages;
@@ -13,17 +15,19 @@ class MessageView extends StatefulWidget {
 class _MessageViewState extends State<MessageView> {
   List<Message> messages = [
     Message(
-        author: "me",
+        author: "482967",
         time: "now",
         content: "hello world",
         zone: "north campus"),
     Message(
-        author: "me",
+        author: "482967",
         time: "now",
         content: "goodbye world",
         zone: "north campus"),
-    Message(author: "me", time: "now", content: "chirp", zone: "north campus"),
-    Message(author: "mason", time: "now", content: "bark", zone: "north campus")
+    Message(
+        author: "111111", time: "now", content: "chirp", zone: "north campus"),
+    Message(
+        author: "222222", time: "now", content: "bark", zone: "north campus")
   ];
   TextEditingController _textController = TextEditingController();
 
@@ -50,31 +54,51 @@ class _MessageViewState extends State<MessageView> {
             child: ListView.builder(
               itemCount: messages.length,
               itemBuilder: (BuildContext context, int index) {
-                return Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Align(
-                    alignment: messages[index].author == 'me'
-                        ? Alignment.centerRight
-                        : Alignment.centerLeft,
-                    child: Container(
-                      padding: EdgeInsets.all(16.0),
-                      decoration: BoxDecoration(
-                        color: messages[index].author == 'me'
-                            ? Color(
-                                0xFF023258) // Set your sent message background color
-                            : Colors
-                                .grey, // Set your received message background color
-                        borderRadius: BorderRadius.circular(16.0),
-                      ),
-                      child: Text(
-                        messages[index].content,
-                        style: TextStyle(
-                          fontSize: 16.0,
-                          color: Colors.white, // Set your message text color
+                return Stack(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Align(
+                        alignment: messages[index].author == 'me'
+                            ? Alignment.centerRight
+                            : Alignment.centerLeft,
+                        child: Container(
+                          padding: EdgeInsets.all(16.0),
+                          decoration: BoxDecoration(
+                            color: messages[index].author == 'me'
+                                ? Color(0xFF023258)
+                                : Colors.grey,
+                            borderRadius: BorderRadius.circular(16.0),
+                          ),
+                          child: Text(
+                            messages[index].content,
+                            style: TextStyle(
+                              fontSize: 16.0,
+                              color: Colors.white,
+                            ),
+                          ),
                         ),
                       ),
                     ),
-                  ),
+                    Positioned(
+                      left: messages[index].author == 'me' ? null : 0,
+                      right: messages[index].author == 'me' ? 0 : null,
+                      bottom: 7,
+                      child: Container(
+                        margin: EdgeInsets.only(
+                          left: messages[index].author == 'me' ? 0 : 8.0,
+                          right: messages[index].author == 'me' ? 8.0 : 0,
+                        ),
+                        width: 12,
+                        height: 12,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color:
+                              getUserColor(int.parse(messages[index].author)),
+                        ),
+                      ),
+                    ),
+                  ],
                 );
               },
             ),
@@ -95,23 +119,23 @@ class _MessageViewState extends State<MessageView> {
               children: [
                 Expanded(
                   child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 8.0, horizontal: 16.0),
-                    child: TextFormField(
+                    padding: EdgeInsets.only(left: 16.0),
+                    child: TextField(
                       controller: _textController,
                       decoration: InputDecoration(
-                          hintText: "type message", border: InputBorder.none),
+                        hintText: 'Type a message',
+                        border: InputBorder.none,
+                      ),
                     ),
                   ),
                 ),
                 IconButton(
                   icon: Icon(Icons.send),
                   onPressed: _sendMessage,
-                  color: Theme.of(context).primaryColor,
-                )
+                ),
               ],
             ),
-          )
+          ),
         ],
       ),
     );
