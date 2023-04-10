e6486f725164e3831b8b8b6d27d9ec0acf366a12
Kevin Le <kl626@cornell.edu>
2023-04-10 00:07:09 -0400

    changed the colors on the home page. Also fixed a bug where text would be stacked on top of the image that it was sent with

--- lib/message_view.dart
+++ lib/message_view.dart
@@ -40,7 +40,7 @@ class _MessageViewState extends State<MessageView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFe5eaee),
+      backgroundColor: Color.fromARGB(255, 0, 0, 0),
       body: Column(
         children: <Widget>[
           Expanded(
@@ -49,71 +49,84 @@ class _MessageViewState extends State<MessageView> {
               itemBuilder: (BuildContext context, int index) {
                 final message = widget.messages[index];
                 final isSentByUser = message.author == widget.userIdentity;
-
-                return Stack(
+                return Column(
                   children: <Widget>[
+                    // The image
                     if (message.media.isNotEmpty)
                       Padding(
                         padding: const EdgeInsets.all(8.0),
                         child: Align(
                           alignment: isSentByUser
-                              ? Alignment.centerRight
-                              : Alignment.centerLeft,
-                          child: SizedBox(
-                            width: MediaQuery.of(context).size.width * 0.7,
-                            child: Image.network(
-                              message.media,
-                              fit: BoxFit.fitWidth,
-                            ),
-                          ),
-                        ),
-                      ),
-                    if (message.content.isNotEmpty)
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Align(
-                          alignment: isSentByUser
-                              ? Alignment.centerRight
-                              : Alignment.centerLeft,
-                          child: Container(
-                            constraints: BoxConstraints(
-                              maxWidth: MediaQuery.of(context).size.width * 0.7,
-                            ),
-                            padding: EdgeInsets.all(16.0),
-                            decoration: BoxDecoration(
-                              color: isSentByUser
-                                  ? Color(0xFF023258)
-                                  : Colors.grey,
-                              borderRadius: BorderRadius.circular(16.0),
-                            ),
-                            child: Text(
-                              message.content,
-                              style: TextStyle(
-                                fontSize: 16.0,
-                                color: Colors.white,
+                              ? Alignment.topRight
+                              : Alignment.topLeft,
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(16.0),
+                            child: SizedBox(
+                              width: MediaQuery.of(context).size.width * .6,
+                              child: Image.network(
+                                width: 300,
+                                message.media,
+                                fit: BoxFit.fitWidth,
                               ),
                             ),
                           ),
                         ),
                       ),
-                    if (message.content.isNotEmpty)
-                      Positioned(
-                        left: isSentByUser ? null : 0,
-                        right: isSentByUser ? 0 : null,
-                        bottom: 7,
-                        child: Container(
-                          margin: EdgeInsets.only(
-                            left: isSentByUser ? 0 : 8.0,
-                            right: isSentByUser ? 8.0 : 0,
+
+                    // This stack contains the message and the profile
+                    Stack(
+                      children: [
+                        // The message
+                        if (message.content.isNotEmpty)
+                          Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Align(
+                              alignment: isSentByUser
+                                  ? Alignment.centerRight
+                                  : Alignment.centerLeft,
+                              child: Container(
+                                constraints: BoxConstraints(
+                                  maxWidth:
+                                      MediaQuery.of(context).size.width * 0.7,
+                                ),
+                                padding: EdgeInsets.all(16.0),
+                                decoration: BoxDecoration(
+                                  color: isSentByUser
+                                      ? Color.fromARGB(255, 111, 185, 211)
+                                      : Color.fromARGB(255, 31, 38, 44),
+                                  borderRadius: BorderRadius.circular(16.0),
+                                ),
+                                child: Text(
+                                  message.content,
+                                  style: TextStyle(
+                                    fontSize: 16.0,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ),
                           ),
-                          width: 12,
-                          height: 12,
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: getUserColor(int.parse(message.author)),
+                        // The profile
+                        if (message.content.isNotEmpty)
+                          Positioned(
+                            left: isSentByUser ? null : 0,
+                            right: isSentByUser ? 0 : null,
+                            bottom: 7,
+                            child: Container(
+                              margin: EdgeInsets.only(
+                                left: isSentByUser ? 0 : 8.0,
+                                right: isSentByUser ? 8.0 : 0,
+                              ),
+                              width: 12,
+                              height: 12,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: getUserColor(int.parse(message.author)),
+                              ),
+                            ),
                           ),
-                        ),
-                      ),
+                      ],
+                    ),
                   ],
                 );
               },
